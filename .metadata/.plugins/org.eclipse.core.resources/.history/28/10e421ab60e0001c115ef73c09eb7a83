810fc873e0082abed2802654611e784808c6ea04
Moura <hiroganja@gmail.com>
2022-05-30 18:38:41 -0300

    003 :)

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/28/10e421ab60e0001c115ef73c09eb7a83
@@ -0,0 +1,14 @@
+package javaBasico;
+
+public class SegundoProgramaJava {
+
+	public static void main(String args[]) {
+		int n1=710;
+		int n2=520;
+		int n3=600;
+		System.out.print("Ola, bem-vindo ao terminal! | ");
+		System.out.print("Valor da variavel n1: "+n1);
+		System.out.print(" | Valor da variavel n2: "+n2);
+		System.out.print(" | Valor da variavel n3: "+n3);
+	}
+}
--- Equipe02/src/javaBasico/SegundoProgramaJava.java
+++ Equipe02/src/javaBasico/SegundoProgramaJava.java
@@ -6,9 +6,6 @@ public class SegundoProgramaJava {
 		int n1=710;
 		int n2=520;
 		int n3=600;
-		System.out.print("Ola, bem-vindo ao terminal! | ");
-		System.out.print("Valor da variavel n1: "+n1);
-		System.out.print(" | Valor da variavel n2: "+n2);
-		System.out.print(" | Valor da variavel n3: "+n3);
+		System.out.print("Ola, bem-vindo ao terminal! | "+"Valor da variavel n1: "+n1+" | Valor da variavel n2: "+n2+" | Valor da variavel n3: "+n3);
 	}
 }
